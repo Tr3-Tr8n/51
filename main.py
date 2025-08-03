4a67c3ecc1030c00a38c1a35ace737be7034fb98
Tr3-Tr8n <tri030411@gmail.com>
2025-08-03 00:46:10 +0000

    everything

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import streamlit as st
-st.title('Dien tin thong tin gioi thieu ban than em')
+
+st.title('Điền thông tin để đăng ký tài khoản')
 my_bar = st.progress(0)
-quiz =  ['Ho va ten:', 'Ngay thang nam sinh:', 'so thich:']
+
+# Danh sách các trường cần nhập
+form_fields = [
+    'Tài khoản:',
+    'Mật khẩu:',
+    'Nhập lại mật khẩu:',
+    'Tên người dùng:',
+    'Email:'
+]
+
 answers = []
-len_quiz = len(quiz)
-for i in range(len_quiz):
-  answer = st.text_input(quiz[i], '')
-  if answer != '':
-    answers.append(answer)
-if st.button('Confirm'):
-  if len(answers) == len_quiz:
-    my_bar.progress(100)
-    st.write('Ban da hoan thanh day du thong tin!')
-    st.balloons()
-  else:
-    my_bar.progress(len(answers)/len_quiz)
-  for i in range(len(answers)):
-    st.write(quiz[i], answers[i])
+total_fields = len(form_fields)
+
+# Vòng lặp để hiển thị từng trường nhập
+for field in form_fields:
+    if 'Mật khẩu' in field:
+        response = st.text_input(field, type='password')
+    else:
+        response = st.text_input(field)
+    if response != '':
+        answers.append(response)
+
+# Nút xác nhận
+if st.button('Xác nhận'):
+    if len(answers) == total_fields:
+        if answers[1] != answers[2]:  # Kiểm tra mật khẩu và nhập lại mật khẩu
+            my_bar.progress(int((len(answers) / total_fields) * 100))
+            st.error("Mật khẩu không khớp!")
+        else:
+            my_bar.progress(100)
+            st.success('Bạn đã hoàn thành đầy đủ thông tin!')
+            st.balloons()
+            for i in range(total_fields):
+                if 'Mật khẩu' not in form_fields[i]:
+                    st.write(form_fields[i], answers[i])
+    else:
+        my_bar.progress(int((len(answers) / total_fields) * 100))
+        st.warning("Vui lòng điền đầy đủ thông tin!")
